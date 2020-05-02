e27800ae66688894f2a24bf3613fa4c62239dd56
PiKaBin <1747320015@qq.com>
2020-05-02 19:33:21 +0800

    to solve cors

--- config/config.default.js
+++ config/config.default.js
@@ -58,7 +58,7 @@ module.exports = appInfo => {
 
 
   config.cors = {
-    origin: '*', // 允许所有跨域访问，注释掉则允许上面 白名单 访问
+    // origin: '*', // 允许所有跨域访问，注释掉则允许上面 白名单 访问
     credentials: true, // 允许跨域请求携带cookies
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
   };
