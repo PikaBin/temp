9d2e47fb118258ea210b0fecd019963b74f364fa
PiKaBin <1747320015@qq.com>
2020-05-05 15:16:07 +0800

    to add update and better struction of files

--- app/model/shoppingBar.js
+++ app/model/shoppingBar.js
@@ -1,16 +1,16 @@
 'use strict';
 
-module.exports = app =>{
-    const mongoose = app.mongoose;
-    const Schema = mongoose.Schema;
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
 
-    const shoppingBarSchema = new Schema({
-        _Id :  Schema.Types.ObjectId,   //购物车商品ID
-        itemId : {type : String, required : true},  //所收藏的服务id
-        partitionId : {type : String, required : true}, //服务分区ID
-        purchaseQuantity : {type : String, required : true},    //购买数量
-        customerId : {type : String, required : true}, //买家id
-        addTime : {type : Date, required : true} //添加时间
-    });
-    return mongoose.model("ShoppingBar",shoppingBarSchema);
-}
+  const shoppingBarSchema = new Schema({
+    _Id: Schema.Types.ObjectId, // 购物车商品ID
+    itemId: { type: String, required: true }, // 所收藏的服务id
+    partitionId: { type: String, required: true }, // 服务分区ID
+    purchaseQuantity: { type: String, required: true }, // 购买数量
+    customerId: { type: String, required: true }, // 买家id
+    addTime: { type: Date, required: true }, // 添加时间
+  });
+  return mongoose.model('ShoppingBar', shoppingBarSchema);
+};
