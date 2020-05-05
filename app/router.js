43205de4097c2d5825069a440c69d1cdeb245874
PiKaBin <1747320015@qq.com>
2020-05-05 15:20:38 +0800

    add some router about item

--- app/router.js
+++ app/router.js
@@ -38,6 +38,7 @@ module.exports = app => {
   router.post('/manager/addtask', controller.item.addTask); // 新增任务
   router.post('/manager/additemImage', controller.item.getPhoto); // 上传单品图片，测试
   router.get('/manager/queryitem', controller.item.queryItem); // 单品查询
+  router.post('/manager/updateitem', controller.item.updateItem); // 单品更新
 
   // 工单管理
   router.get('/manager/neworder', controller.workorder.findOrder); // 返回新增订单
