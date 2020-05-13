476dcd549fb09d45e8cbec65b0a287b1216eb4e9
PiKaBin <1747320015@qq.com>
2020-05-13 23:14:36 +0800

    add two attr

--- app/model/verify/news.js
+++ app/model/verify/news.js
@@ -7,6 +7,8 @@ module.exports = app => {
   const mongoose = app.mongoose;
   const Schema = mongoose.Schema;
   const NewsSchema = new Schema({
+    object: { type: String, required: true }, // 对象标识
+    action: String, // 动作标识
     read: { type: String, default: '0' }, // 是否已读，0表示未读，1表示已读
     reason: { type: String }, // 审核 产生的理由
     auditorID: { type: String }, // 审核员ID
